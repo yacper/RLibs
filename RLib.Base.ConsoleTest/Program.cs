92e58e7d93ad3f0aea3589babb0f9126c7613058
yacper <yacper@gmail.com>
2022-07-22 12:06:16 +0800

    ExecuteInUIThread

---
+++ RLib.Base.ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RLib.Base;
+
+Console.WriteLine($"MainThread:{Thread.CurrentThread.ManagedThreadId}");
+
+Thread t = new Thread(() =>
+{
+    Console.WriteLine($"Work Thread:{Thread.CurrentThread.ManagedThreadId}");
+    MainThreadEx.ExecuteInMainThread(() =>
+    {
+        Console.WriteLine($"Thread:{Thread.CurrentThread.ManagedThreadId}");
+    });
+});
+t.Start();
+
+
+Console.Read();
---
+++ RLib.Base/Threading/MainThreadEx.cs
@@ -0,0 +1,39 @@
+﻿// created: 2022/07/22 11:54
+// author:  rush
+// email:   yacper@gmail.com
+// 
+// purpose:
+// modifiers:
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RLib.Base;
+
+public static class MainThreadEx
+{
+    public static void ExecuteInUIThread(this Action action)        // 在ui线程中运行，wpf/winform有效，console错误
+    {
+        // for Console Application Task will be scheduled to any available thread in ThreadPool ,
+        // but for WPF and Winform it will be 1 if Managed ID of Thread is 1
+
+        var synchronization = SynchronizationContext.Current;
+        if (synchronization != null)
+        {
+            synchronization.Send(_ => action(), null);//sync
+            //OR
+            synchronization.Post(_ => action(), null);//async
+        }
+        else
+            Task.Factory.StartNew(action); // console下的勉强之策
+
+        //var scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+        //Task task = new Task(action);
+        //if (scheduler != null)
+        //    task.Start(scheduler);
+        //else
+        //    task.Start();
+    }
+}

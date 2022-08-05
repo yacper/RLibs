f5094ae73513f3ce395d8ed36d1e3c170276a528
yacper <yacper@gmail.com>
2022-08-05 16:06:26 +0800

    .

---
+++ RLib.Base/Exceptions/ExceptionEx.cs
@@ -0,0 +1,22 @@
+﻿/********************************************************************
+    created:	2022/8/5 10:30:55
+    author:		rush
+    email:		yacper@gmail.com	
+	
+    purpose:
+    modifiers:	
+*********************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLib.Base.Exceptions
+{
+    public static class ExceptionEx
+    {
+
+
+    }
+}

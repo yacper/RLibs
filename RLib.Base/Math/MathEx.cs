2317db3827a0eda1f7ecda9592720d5ff7fe2d2e
yacper <yacper@gmail.com>
2023-03-07 17:18:28 +0800

    笛卡尔积

--- RLib.Base/Math/MathEx.cs
+++ RLib.Base/Math/MathEx.cs
@@ -958,5 +958,18 @@ namespace RLib.Base
         {
             return (0 < val) ? (int)(val + 0.5) : (int)(val - 0.5);
         }
+
+
+       // 笛卡尔积 https://ericlippert.com/2010/06/28/computing-a-cartesian-product-with-linq/
+       public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
+       {
+           IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+           return sequences.Aggregate(
+                                      emptyProduct,
+                                      (accumulator, sequence) =>
+                                          from accseq in accumulator
+                                          from item in sequence
+                                          select accseq.Concat(new[] { item }));
+       }
     }
 }

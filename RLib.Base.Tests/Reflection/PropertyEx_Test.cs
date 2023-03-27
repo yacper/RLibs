0a0a34fa80d6ba1b02c871b8628fd4eb078396ca
yacper <yacper@gmail.com>
2023-03-27 14:32:35 +0800

    .

--- RLib.Base.Tests/Reflection/PropertyEx_Test.cs
+++ RLib.Base.Tests/Reflection/PropertyEx_Test.cs
@@ -11,10 +11,26 @@ namespace RLib.Base.Tests.Reflection;
 
 public class PropertyEx_Test
 {
+    public interface IA
+    {
+        int p1 { get; set; }
+        
+    }
+
+    public class A:IA
+    {
+
+        public int p1 { get; set; }
+        public int p2 { get; set; }
+    }
+
+
+
     [Test]
     public void Test()
     {
-      
+        var pi = typeof(A).GetProperty(nameof(IA.p1));
+
     }
 
 

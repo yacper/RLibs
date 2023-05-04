a45965f1c1941fd7983585d951f5451b3cf790ae
yacper <yacper@gmail.com>
2023-05-04 19:03:28 +0800

    .

---
+++ RLib.Base.Tests/Utils/TimespanEx_Test.cs
@@ -0,0 +1,34 @@
+﻿// created: 2023/05/04 17:58
+// author:  rush
+// email:   yacper@gmail.com
+// 
+// purpose:
+// modifiers:
+
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using RLib.Base.Utils;
+
+namespace RLib.Base.Tests.Utils;
+
+public class TimespanEx_Test
+{
+    [Test]
+    public void Test()
+    {
+        TimeSpan.Parse("00:00:00").IsTimeofday().Should().Be(true);
+        TimeSpan.Parse("20:00:00").IsTimeofday().Should().Be(true);
+        TimeSpan.Parse("24:00:00").IsTimeofday().Should().Be(false);
+
+        TimeSpan.Parse("25:0:0").IsTimeofday().Should().Be(false);
+
+
+        TimeSpan.Parse("23:0:0").IsWithin(TimeSpan.Parse("20:0:0"), TimeSpan.Parse("23:55:00")).Should().Be(true);
+        TimeSpan.Parse("23:0:0").IsWithin(TimeSpan.Parse("20:0:0"), TimeSpan.Parse("0:00:00")).Should().Be(true);
+        TimeSpan.Parse("23:0:0").IsWithin(TimeSpan.Parse("20:0:0"), TimeSpan.Parse("2:00:00")).Should().Be(true);
+        TimeSpan.Parse("19:0:0").IsWithin(TimeSpan.Parse("20:0:0"), TimeSpan.Parse("2:00:00")).Should().Be(false);
+
+    }
+
+}
---
+++ RLib.Base/Utils/TimespanEx.cs
@@ -0,0 +1,42 @@
+﻿// created: 2023/05/04 15:59
+// author:  rush
+// email:   yacper@gmail.com
+// 
+// purpose:
+// modifiers:
+
+using System;
+using FluentAssertions;
+
+namespace RLib.Base.Utils;
+
+public static class TimespanEx
+{
+    // 作为24小时制， [00:00 ~ 23:59:59)
+    public static bool IsTimeofday(this TimeSpan ts)
+    {
+        var ticks = ts.Ticks;
+        return ticks >= 0 && ticks <= TimeSpan.TicksPerDay;
+    }
+
+
+    // 作为24小时制， [00:00 ~ 23:59:59)
+    // 可以跨一天 比如20:00 ~ 02:00 
+    public static bool IsWithin(this TimeSpan ts, TimeSpan start, TimeSpan end)
+    {
+        ts.IsTimeofday().Should().Be(true);
+        start.IsTimeofday().Should().Be(true);
+        end.IsTimeofday().Should().Be(true);
+
+        if (end >= start)
+        {
+            return ts >= start && ts <= end;
+        }
+        else
+        {
+            return ts >= start || ts <= end;
+        }
+    }
+
+    
+}

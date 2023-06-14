adecb59690182a10d558aabbe8f278418b1b2b93
yacper <yacper@gmail.com>
2023-06-14 19:24:11 +0800

    json test

--- RLib.Base.Tests/3rdHelpers/JsonExTest.cs
+++ RLib.Base.Tests/3rdHelpers/JsonExTest.cs
@@ -5,6 +5,11 @@
 // purpose:
 // modifiers:
 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using NUnit.Framework;
 
 namespace RLib.Base.Tests._3rdHelpers;
@@ -14,7 +19,78 @@ public struct AA
     public int    A { get; set; } 
     public string B { get; set; }
 }
+public struct BB
+{
+    public int    A { get; set; } 
+    public string C { get; set; }
+}
 
+
+public class PropertyRenameAndIgnoreSerializerContractResolver : DefaultContractResolver
+{
+    private readonly Dictionary<Type, HashSet<string>> _ignores;
+    private readonly Dictionary<Type, Dictionary<string, string>> _renames;
+
+    public PropertyRenameAndIgnoreSerializerContractResolver()
+    {
+        _ignores = new Dictionary<Type, HashSet<string>>();
+        _renames = new Dictionary<Type, Dictionary<string, string>>();
+    }
+
+    public void IgnoreProperty(Type type, params string[] jsonPropertyNames)
+    {
+        if (!_ignores.ContainsKey(type))
+            _ignores[type] = new HashSet<string>();
+
+        foreach (var prop in jsonPropertyNames)
+            _ignores[type].Add(prop);
+    }
+
+    public void RenameProperty(Type type, string propertyName, string newJsonPropertyName)
+    {
+        if (!_renames.ContainsKey(type))
+            _renames[type] = new Dictionary<string, string>();
+
+        _renames[type][propertyName] = newJsonPropertyName;
+    }
+
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        var property = base.CreateProperty(member, memberSerialization);
+
+        if (IsIgnored(property.DeclaringType, property.PropertyName))
+        {
+            property.ShouldSerialize = i => false;
+            property.Ignored = true;
+        }
+
+        if (IsRenamed(property.DeclaringType, property.PropertyName, out var newJsonPropertyName))
+            property.PropertyName = newJsonPropertyName;
+
+        return property;
+    }
+
+    private bool IsIgnored(Type type, string jsonPropertyName)
+    {
+        if (!_ignores.ContainsKey(type))
+            return false;
+
+        return _ignores[type].Contains(jsonPropertyName);
+    }
+
+    private bool IsRenamed(Type type, string jsonPropertyName, out string newJsonPropertyName)
+    {
+        Dictionary<string, string> renames;
+
+        if (!_renames.TryGetValue(type, out renames) || !renames.TryGetValue(jsonPropertyName, out newJsonPropertyName))
+        {
+            newJsonPropertyName = null;
+            return false;
+        }
+
+        return true;
+    }
+}
 
 public class JsonExTest
 {
@@ -23,8 +99,40 @@ public class JsonExTest
     {
         AA a = new AA() { A = 1, B = "hello" };
         a.DumpJson();
-
     }
 
+
+ [Test]
+    public void Test_2()
+    {
+        List<AA> aa = new()
+        {
+            new AA() { A = 1, B = "hello" },
+            new AA() { A = 2, B = "hello" },
+            new AA() { A = 3, B = "hello" },
+        };
+
+        var      astr = aa.ToJson();
+        //List<BB> bb   = astr.ToJsonObj<List<BB>>();
+
+        var jsonResolver = new PropertyRenameAndIgnoreSerializerContractResolver();
+        //jsonResolver.IgnoreProperty(typeof(Person), "Title");
+        jsonResolver.RenameProperty(typeof(AA), "B", "D");
+
+        var serializerSettings = new JsonSerializerSettings();
+        serializerSettings.ContractResolver = jsonResolver;
+
+        var json = JsonConvert.SerializeObject(aa, serializerSettings);
+
+        var jsonResolver2 = new PropertyRenameAndIgnoreSerializerContractResolver();
+        //jsonResolver.IgnoreProperty(typeof(Person), "Title");
+        jsonResolver.RenameProperty(typeof(BB), "C", "D");
+
+        var serializerSettings2 = new JsonSerializerSettings();
+        serializerSettings2.ContractResolver = jsonResolver2;
+
+        var bb = JsonConvert.DeserializeObject<List<BB>>(json, serializerSettings2);
+
+    }
 
 }
